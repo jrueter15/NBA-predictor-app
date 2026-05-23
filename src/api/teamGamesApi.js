a920f49a6743bfff6e2c5ea284c45915bd928c1c
Jared <jrueter15@gmail.com>
2026-05-23 16:14:41 -0500

    Started a function for returning a teams last 10 games

---
+++ src/api/teamGamesApi.js
@@ -0,0 +1,13 @@
+export async function getLast10Games(teamId) {
+  const response = await fetch(
+    `https://api.balldontlie.io/v1/games?team_ids[]=${teamId}&per_page=10`,
+    {
+      headers: {
+        Authorization: import.meta.env.VITE_BALLDONTLIE_KEY
+      }
+    }
+  );
+
+  const data = await response.json();
+  return data.data;
+}
